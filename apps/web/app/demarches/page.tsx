b13f19d334b7c69d5eb18cb405946922e9671c3d
Madou <madouyatt95@gmail.com>
2025-12-16 13:44:08 +0100

    style: reduce demarches page padding for full screen layout

--- apps/web/app/demarches/page.tsx
+++ apps/web/app/demarches/page.tsx
@@ -4,18 +4,14 @@
 
 import { DemarchesView } from '@/components/DemarchesView';
 import { Sidebar } from '@/components/Sidebar';
-import { Header } from '@/components/Header';
 import { MobileNav } from '@/components/MobileNav';
 
 export default function DemarchesPage() {
     return (
         <div className="app-container">
             <Sidebar />
-            <main className="flex-1 flex flex-col">
-                <Header title="Mes Démarches" />
-                <div className="main-content">
-                    <DemarchesView />
-                </div>
+            <main className="flex-1 flex flex-col" style={{ overflow: 'auto' }}>
+                <DemarchesView />
             </main>
             <MobileNav />
         </div>
